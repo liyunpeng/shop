d9324e3459e2f68014587b75f62ee96ae6ef27f1
Your Name <kylin27@126.com>
2020-05-10 22:37:49 +0800

    add micro

---
+++ micro/user/go.mod
@@ -0,0 +1,19 @@
+module shopping/user
+
+go 1.13
+
+require (
+	github.com/denisenkom/go-mssqldb v0.0.0-20190315220205-a8ed825ac853 // indirect
+	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/golang/protobuf v1.4.1
+	github.com/jinzhu/gorm v1.9.2
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+	github.com/jinzhu/now v1.0.0 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
+	github.com/micro/go-config v0.14.0
+	github.com/micro/go-log v0.1.0
+	github.com/micro/go-micro v1.16.0
+	github.com/micro/go-micro/v2 v2.6.0
+	golang.org/x/crypto v0.0.0-20200323165209-0ec3e9974c59
+)
---
+++ micro/user/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/micro/go-config"
+	"github.com/micro/go-log"
+	"github.com/micro/go-micro/v2"
+	"shopping/user/handler"
+	"shopping/user/model"
+	"shopping/user/repository"
+
+	user "shopping/user/proto/user"
+)
+
+func main() {
+
+	//加载配置项
+	err := config.LoadFile("./config.json")
+	if err != nil {
+		log.Fatalf("Could not load config file: %s", err.Error())
+		return
+	}
+	conf := config.Map()
+
+	//db
+	db, err := CreateConnection(conf["mysql"].(map[string]interface{}))
+	defer db.Close()
+
+	db.AutoMigrate(&model.User{})
+
+	if err != nil {
+		log.Fatalf("connection error : %v \n" , err)
+	}
+
+	repo := &repository.User{db}
+
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.srv.user"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	user.RegisterUserServiceHandler(service.Server(), &handler.User{repo})
+
+	// Register Struct as Subscriber
+	//micro.RegisterSubscriber("go.micro.srv.user", service.Server(), new(subscriber.Example))
+
+	// Register Function as Subscriber
+	//micro.RegisterSubscriber("go.micro.srv.user", service.Server(), subscriber.Handler)
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
